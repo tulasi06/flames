c98dc3772d3c3ef75503b709c6b8080660613f6f
tulasi06 <tulasi.seelamkurthi@gmail.com>
2016-09-17 15:40:29 +0200

    Initial empty project with input fields

---
+++ app/js/MainController.js
@@ -0,0 +1,11 @@
+var app = angular.module('flames', []);
+app.controller('myController', function($scope) {
+
+    $scope.determineRelationship = function()
+    {
+
+        //the magic will happen here
+       
+    }
+    $scope.determineRelationship();
+});
